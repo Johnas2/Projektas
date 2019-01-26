7f366429e270e7a75b8c5bc74c149066a70a06a0
DjMeska <meskauskasjustinas@gmail.com>
2019-01-26 12:46:29 +0200

    Hell

--- Failiukas/Failiukas/Source.cpp
+++ Failiukas/Failiukas/Source.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world!" << endl;
-	system("pause");
-	return 0;
+	int i;
+	for (i = -1; i > 1; i--)
+	{
+		cout << "Hello world!" << endl;
+	}
+	char *obtain_data()
+
+		; {
+		
+		       char *buf = malloc(100);
+		        FILE *f;
+		        size_t length = 0;
+		
+			       if (!buf) return NULL;
+		       f = fopen("football.dat", "r");
+		       if (!f) return NULL;
+		 * buf = '\0';
+		       while (fgets(buf + length, 100, f))
+			           {
+			           length = strlen(buf);
+			           realloc(buf, length + 100);
+			           }
+	       (void)fclose(f);
+		       return buf;
+	   }
+	
 }
